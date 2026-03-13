957b433978a88575fd29a2a2c00133385cbaf1ac
swan27768 <oljushka.one@gmail.com>
2026-03-13 23:58:21 +0200

    Muokattu navbar

---
+++ js/navbar.js
@@ -0,0 +1,12 @@
+<script>
+
+const toggle = document.getElementById("menuToggle");
+const menu = document.getElementById("navMenu");
+
+toggle.addEventListener("click", function(){
+
+menu.classList.toggle("active");
+
+});
+
+</script>
