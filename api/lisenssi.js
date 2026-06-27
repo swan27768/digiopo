81234960987559e706d2c2c48303110896c08567
Olga <digiopo@digiopo.fi>
2026-06-28 00:40:24 +0300

    Korjattu taustapäivitysbugi: tallennetaan koodi localStorageen

--- api/lisenssi.js
+++ api/lisenssi.js
@@ -102,6 +102,7 @@ export default async function handler(req, res) {
 
     return res.status(200).json({
       ok: true,
+      koodi: lisenssi.koodi,
       voimassa_asti: lisenssi.voimassa_asti,
       koulu: lisenssi.koulu,
     });
